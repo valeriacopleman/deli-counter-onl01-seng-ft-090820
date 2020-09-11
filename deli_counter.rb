9cf8f1d6a8f7e4e0ae0cd344ed2219bf17f845fb
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-11 05:14:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line(katz_deli)
+  
+  current_line=[]
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+     counter = 1 
+     katz_deli.each do |name|
+       current_line<< "#{counter}. #{name}"
+       counter += 1 
+     end
+  
+  puts "The line is currently: #{current_line.join(" ")}"
+end
+end
+
+def take_a_number(katz_deli)
+  number =1 
+  
+  katz_deli.each do |customer|
+    puts "Welcome, #{customer}. You are number #{number} in line."
+    number += 1 
+  end
+ end
+ 
