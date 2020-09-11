c2578787ce1fe59bb87ed5dbfd4f7503f2ac8bfc
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-11 05:23:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,12 +16,13 @@ def line(katz_deli)
 end
 end
 
-def take_a_number(katz_deli)
-  customer = gets.chomp
+def take_a_number(katz_deli, customer)
+  
   katz_deli<< customer 
     puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
    
   end
+ 
  
  
  
